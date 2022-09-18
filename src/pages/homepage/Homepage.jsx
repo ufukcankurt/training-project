2b018cf080d4b185f26719b1126301ee81ea9924
ufukcankurt <ufuk.cnk@gmail.com>
2022-09-18 06:21:08 +0300

    feat(homepage): add a new statistic section

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -7,23 +7,27 @@ import WhoWeAre from "../../components/whoWeAre/WhoWeAre";
 import PopularClasses from "../../components/popularClasses/PopularClasses";
 import OurTrainer from "../../components/ourTrainer/OurTrainer";
 import OurPricing from "../../components/ourPricing/OurPricing";
+import Statistic from "../../components/statistic/Statistic";
 
 const Homepage = () => {
   return (
     <div className={`${styles.container}`}>
       <HomeNav />
       <HomeBanner />
-      <section className={`${styles.firstSection} section`}>
+      <section className={`section`}>
         <WhoWeAre />
       </section>
       <section className={`${styles.secondSection} section`}>
         <PopularClasses />
       </section>
-      <section className={`${styles.thirdSection} section`}>
+      <section className={`section`}>
         <OurTrainer />
       </section>
       <section className={`${styles.fourthSection} section`}>
         <OurPricing />
+      </section>
+      <section>
+        <Statistic className={`${styles.fifthSection} section`} />
       </section>
     </div>
   );
