7bb5a4f69fa20bd7243e2c575763f69a6f093a91
Ufuk Can KURT <ufuk.cnk@gmail.com>
2023-01-28 18:47:52 +0300

    feat(components): create comment component

---
+++ src/components/comment/Comment.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import styles from "./comment.module.css";
+
+const Comment = ({comment}) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.pp}>
+        <img src={comment?.img} alt="profile pic" />
+      </div>
+      <div className={styles.text}>
+        <p>
+          {comment?.text}
+        </p>
+      </div>
+      <div className={styles.svg}>
+        <img src="/img/quotation-marks.svg" alt="" />
+      </div>
+      <div className={styles.username}>
+        {comment?.name}
+      </div>
+    </div>
+  )
+}
+
+export default Comment
