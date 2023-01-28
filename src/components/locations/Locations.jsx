6e7c680a95a043cc899ef4482e8009efc2e2fefc
Ufuk Can KURT <ufuk.cnk@gmail.com>
2023-01-28 18:47:34 +0300

    feat(components): create locations component

---
+++ src/components/locations/Locations.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import styles from './locations.module.css'
+
+import SectionTitle from '../../common/sectionTitle/SectionTitle';
+import Location from '../location/Location';
+
+
+const pageInfos = {
+  firstTitle: "Locations | ",
+  secondTitle: "Classes",
+  description:
+    "60 minutes full body workout led by fitness experts who use a mixture of dumbbells, bands, battle ropes, sandbags, sliders, pull-up bars, body weight and HIIT. The aim is to build both your strength and cardiovascular endurance.",
+};
+
+const LOCATIONS = [
+  {
+    title: "Outdoor Sessions",
+    location: "Grovelands Park",
+    src: "https://maps.google.com/maps?q=Grovelands%20Park&t=&z=13&ie=UTF8&iwloc=&output=embed",
+    date: "Saturday 09:30 - 10:30",
+  },
+  {
+    title: "Indoor Sessions",
+    location: "Palmers Green United Reformed Church Hall",
+    src: "https://maps.google.com/maps?q=Palmers%20Green%20United%20Reformed%20Church%20Hall&t=&z=13&ie=UTF8&iwloc=&output=embed",
+    date: "Monday-Wednesday 19:30 - 20:30 & Friday 07:00 - 07:45",
+  }
+]
+
+const Locations = () => {
+  return (
+    <div className={styles.container}>
+      <SectionTitle pageInfos={pageInfos} />
+      <div className={styles.maps}>
+      {
+        LOCATIONS.map((location, index) =>
+          <Location key={index} location={location} />
+        )
+      }
+      </div>
+    </div>
+  )
+}
+
+export default Locations
