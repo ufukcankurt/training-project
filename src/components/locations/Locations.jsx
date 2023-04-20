319b5f670f45354bb3a729dee54fc2507fce9f2c
Ufuk Can KURT <ufuk.cnk@gmail.com>
2023-04-21 01:43:26 +0300

    feat(locations): add new map infos

--- src/components/locations/Locations.jsx
+++ src/components/locations/Locations.jsx
@@ -24,7 +24,13 @@ const LOCATIONS = [
     location: "Palmers Green United Reformed Church Hall",
     src: "https://maps.google.com/maps?q=Palmers%20Green%20United%20Reformed%20Church%20Hall&t=&z=13&ie=UTF8&iwloc=&output=embed",
     date: "Monday-Wednesday 19:30 - 20:30 & Friday 07:00 - 07:45",
-  }
+  },
+  {
+    title: "Outdoor sessions",
+    location: "Enfield Town Park",
+    src: "https://www.google.com/maps/embed/v1/place?q=51.6485547,-0.0861552&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8",
+    date: "Sunday 09:30-10:30",
+  },
 ]
 
 const Locations = () => {
@@ -32,11 +38,11 @@ const Locations = () => {
     <div className={styles.container}>
       <SectionTitle pageInfos={pageInfos} />
       <div className={styles.maps}>
-      {
-        LOCATIONS.map((location, index) =>
-          <Location key={index} location={location} />
-        )
-      }
+        {
+          LOCATIONS.map((location, index) =>
+            <Location key={index} location={location} />
+          )
+        }
       </div>
     </div>
   )
