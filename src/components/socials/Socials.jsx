eceaf0041a668a1dc73fd145a915f513dac36d74
Ufuk Can KURT <ufuk.cnk@gmail.com>
2023-05-11 09:06:36 +0300

    refactor(socials-footer): update social links

--- src/components/socials/Socials.jsx
+++ src/components/socials/Socials.jsx
@@ -3,31 +3,34 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faFacebookSquare,
-  faTwitter,
   faInstagram,
   faYoutube,
-  faLinkedin,
+  faWhatsapp
 } from "@fortawesome/free-brands-svg-icons";
 
 const Socials = () => {
   return (
     <div className={styles.container}>
-      <div className={`${styles.item} ${styles.facebook}`}>
-        <FontAwesomeIcon icon={faFacebookSquare} className={styles.icon} />
-      </div>
-      <div className={`${styles.item} ${styles.twitter}`}>
-        <FontAwesomeIcon icon={faTwitter} className={styles.icon} />
-      </div>
-      <div className={`${styles.item} ${styles.instagram}`}>
-        <FontAwesomeIcon icon={faInstagram} className={styles.icon} />
-      </div>
-
-      <div className={`${styles.item} ${styles.linkedin}`}>
-        <FontAwesomeIcon icon={faLinkedin} className={styles.icon} />
-      </div>
-      <div className={`${styles.item} ${styles.youtube}`}>
-        <FontAwesomeIcon icon={faYoutube} className={styles.icon} />
-      </div>
+      <a href="https://www.facebook.com/greatbearfitnessuk" target="_blank" rel="noreferrer">
+        <div className={`${styles.item} ${styles.facebook}`}>
+          <FontAwesomeIcon icon={faFacebookSquare} className={styles.icon} />
+        </div>
+      </a>
+      <a href="https://instagram.com/greatbearfitnessuk/" target="_blank" rel="noreferrer">
+        <div className={`${styles.item} ${styles.instagram}`}>
+          <FontAwesomeIcon icon={faInstagram} className={styles.icon} />
+        </div>
+      </a>
+      <a href="https://www.youtube.com/@GreatBearFitnessUK" target="_blank" rel="noreferrer">
+        <div className={`${styles.item} ${styles.youtube}`}>
+          <FontAwesomeIcon icon={faYoutube} className={styles.icon} />
+        </div>
+      </a>
+      <a href="https://wa.me/message/LLYCPWMPWGFFN1"target="_blank" rel="noreferrer">
+        <div className={`${styles.item} ${styles.whatsapp}`}>
+          <FontAwesomeIcon icon={faWhatsapp} className={styles.icon} />
+        </div>
+      </a>
     </div>
   );
 };
