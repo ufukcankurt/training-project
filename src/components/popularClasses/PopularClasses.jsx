000732f7d63c74a48326713d982c9f8aea08c41c
Ufuk Can KURT <ufuk.cnk@gmail.com>
2023-05-27 10:03:12 +0300

    refactor(popular-classes): update images

--- src/components/popularClasses/PopularClasses.jsx
+++ src/components/popularClasses/PopularClasses.jsx
@@ -12,21 +12,21 @@ const pageInfos = {
 
 const items = [
   {
-    image: "./img/join-26.jpg",
+    image: "./img/first1.jpg",
     title: "crosfit",
     description:
       "Curabitur fermentum odio ac enim interdum consequat. Sed vel faucibus leo. Ut turpis ipsum, fringilla eget tortor ...",
     price: "Free",
   },
   {
-    image: "./img/join-2.jpg",
+    image: "./img/first2.jpg",
     title: "AEROBIC",
     description:
       "Curabitur fermentum odio ac enim interdum consequat. Sed vel faucibus leo. Ut turpis ipsum, fringilla eget tortor ...",
     price: "Free",
   },
   {
-    image: "./img/join-3.jpg",
+    image: "./img/first3.jpg",
     title: "GYM AND FITNESS",
     description:
       "Curabitur fermentum odio ac enim interdum consequat. Sed vel faucibus leo. Ut turpis ipsum, fringilla eget tortor ...",
@@ -40,7 +40,7 @@ const items = [
     price: "$20",
   },
   {
-    image: "./img/join-1.jpg",
+    image: "./img/first4.jpg",
     title: "ZUMBA",
     description:
       "Curabitur fermentum odio ac enim interdum consequat. Sed vel faucibus leo. Ut turpis ipsum, fringilla eget tortor ...",
@@ -60,13 +60,13 @@ const PopularClasses = () => {
     <div className={styles.container}>
       <SectionTitle pageInfos={pageInfos} />
       <div className={styles.content}>
-        <img src="./img/join.jpg" alt="" />
-        {/* {items.map((item, i) => (
+        {/* <img src="./img/join.jpg" alt="" /> */}
+        {items.map((item, i) => (
           <div className={styles.item}>
             <div className={styles.image}>
               <img src={item.image} alt="" />
             </div>
-            <div className={styles.inner}>
+            {/* <div className={styles.inner}>
               <div className={styles.header}>
                 <h4 className={styles.h4}>
                   <p className={styles.title}>{item.title}</p>
@@ -81,9 +81,9 @@ const PopularClasses = () => {
                   <span>{item.price}</span>
                 </div>
               </div>
-            </div>
+            </div> */}
           </div>
-        ))} */}
+        ))}
       </div>
     </div>
   );
